505efd1b7c1bcc0184027b447b75d33f0aca0b43
researchdevelopment <fotouhiarian@gmail.com>
2025-04-02 10:49:52 -0400

    v2.9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,16 @@
-// import { NgModule } from '@angular/core';
-// import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';  // Add HashLocationStrategy
 
-// const routes: Routes = [];
-
-// @NgModule({
-//   imports: [RouterModule.forRoot(routes)],
-//   exports: [RouterModule]
-// })
-// export class AppRoutingModule { }
-
-
-
-import { Routes } from '@angular/router';
-
-import { AppComponent } from './app.component';
+// Import your components
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { CertificatesComponent } from './certificates/certificates.component';
 import { ProjectsComponent } from './projects/projects.component';
 
-export const appRoutes: Routes = [
+// Define your routes
+const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'skills', component: SkillsComponent },
@@ -30,3 +19,12 @@ export const appRoutes: Routes = [
   { path: '', redirectTo: '', pathMatch: 'full' },
   { path: '**', component: HomeComponent }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }  // Use HashLocationStrategy here
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { SkillsComponent } from './skills/skills.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { CertificatesComponent } from './certificates/certificates.component';
-import { ProjectsComponent } from './projects/projects.component';
+import { NavbarComponent } from './navbar/navbar.component';  // Import NavbarComponent
+import { FooterComponent } from './footer/footer.component';  // Import FooterComponent
 
-import { Routes } from '@angular/router';
-import { RouterModule } from '@angular/router';
-// import { routes } from './app.routes';
-
-import { appRoutes } from './app-routing.module';
-import { FooterComponent } from './footer/footer.component';
-
-
-// import { NgwWowModule } from 'ngx-wow';
-
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent,
-    SkillsComponent,
-    NavbarComponent,
-    CertificatesComponent,
-    ProjectsComponent,
-    FooterComponent
+    NavbarComponent,  // Declare NavbarComponent
+    FooterComponent   // Declare FooterComponent
   ],
   imports: [
     BrowserModule,
-    // AppRoutingModule.forRoot(appRoutes)
-    RouterModule.forRoot(appRoutes)
-
-
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
